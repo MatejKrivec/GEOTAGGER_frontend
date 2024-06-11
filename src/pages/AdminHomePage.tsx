1a35bcea22978b31776a563f45f5f5adba2c251d
MatejKrivec <matej.krivec6@gmail.com>
2024-06-11 18:25:54 +0200

    popravek2

--- src/pages/AdminHomePage.tsx
+++ src/pages/AdminHomePage.tsx
@@ -6,7 +6,7 @@ import { toast, ToastContainer } from 'react-toastify';
 import ActivityLogItem from './UserActivity/ActivityLogItem';
 import Cookies from 'js-cookie';
 import Activity from '../assets/Interfaces/Activity';
-import logoImage from  '../assets/images/geotagger_logo.png';
+import logoPic from  '../assets/images/geotagger_logo.png';
 
 
 
@@ -112,7 +112,7 @@ const AdminHomePage: React.FC = () => {
       <div className="main m-5">
         <div className="headerContainer flex justify-between items-center">
           <div className="logoContainer">
-            <img src={logoImage} alt="logo" />
+            <img src={logoPic} alt="logo" />
           </div>
           <div className="Header-container flex items-center">
             <button
